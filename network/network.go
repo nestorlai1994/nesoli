b4ff2ec6b262a7226b93cfdc4a3a3d4124cb1216
Nestor.LAI <a0imperiality@gmail.com>
2024-09-03 15:26:07 +0800

    [network]add showlog option and IsUdpListening function

--- network/network.go
+++ network/network.go
@@ -7,13 +7,28 @@ import (
 	"github.com/nestorlai1994/nesoli/logger"
 )
 
-func IsPortListening(port int, proto string) (bool, error) {
+func IsPortListening(port int, proto string, showlog bool) (bool, error) {
 	// Check if port is listening
 	ln, err := net.Listen(proto, fmt.Sprintf(":%d", port))
 	if err != nil {
-		logger.Info("Port is listening: " + fmt.Sprintf(":%d", port))
+		if showlog {
+			logger.Info("Port is listening: " + fmt.Sprintf(":%d", port))
+		}
 		return true, nil
 	}
 	defer ln.Close()
 	return false, fmt.Errorf("port is not listening")
+}
+
+func IsUdpListening(port int, proto string, showlog bool) (bool, error) {
+	// Check if DHCP service is running
+	conn, err := net.ListenPacket(proto, fmt.Sprintf(":%d", port))
+	if err != nil {
+		if showlog {
+			logger.Info(fmt.Sprintf("%d/%s service is running", port, proto))
+		}
+		return true, nil
+	}
+	defer conn.Close()
+	return false, fmt.Errorf("%d/%s service is not running", port, proto)
 }
--- network/network_test.go
+++ network/network_test.go
@@ -1,12 +1,15 @@
 package network
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestIsPortListening(t *testing.T) {
 	t.Run("Port 22 is listening", func(t *testing.T) {
 		port := 22
 		proto := "tcp"
-		got, err := IsPortListening(port, proto)
+		got, err := IsPortListening(port, proto, true)
 		if err != nil {
 			t.Errorf("IsPortListening() error = %v", err)
 			return
@@ -19,13 +22,38 @@ func TestIsPortListening(t *testing.T) {
 	t.Run("Port is not listening", func(t *testing.T) {
 		port := 65535
 		proto := "tcp"
-		got, err := IsPortListening(port, proto)
+		got, err := IsPortListening(port, proto, true)
 		if err == nil {
 			t.Errorf("IsPortListening() error = %v", err)
 			return
 		}
 		if got != false {
 			t.Errorf("IsPortListening() = %v, want %v", got, false)
+		}
+	})
+}
+
+func TestIsDHCPPortOpen(t *testing.T) {
+	t.Run("Port 67 is open", func(t *testing.T) {
+		got, err := IsUdpListening(67, "udp", true)
+		if err != nil {
+			t.Errorf("IsPortOpen() error = %v", err)
+		}
+		if !got {
+			t.Errorf("IsPortOpen() = %v, want %v", got, true)
+		}
+	})
+}
+
+func TestIsTFTPServiceListening(t *testing.T) {
+	t.Run("TFTP service is not listening", func(t *testing.T) {
+		got, err := IsUdpListening(69, "udp", true)
+		if err == nil {
+			t.Errorf("IsTFTPServiceListening() error = %v", fmt.Errorf("TFTP service is listening"))
+			return
+		}
+		if got {
+			t.Errorf("IsTFTPServiceListening() = %v, want %v", got, false)
 		}
 	})
 }
